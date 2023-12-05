68956caa7f6fd085140c0fed26dfe4c6b393a36b
thomaskitaba <thomaskitaba@gmail.com>
2023-12-05 17:26:28 +0300

    updated

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+""" you have n number of locked boxes in front of you.
+    Each box is numbered sequentially from 0 to n - 1
+    and each box may contain keys to the other boxes.
+"""
+
+
 def canUnlockAll(boxes):
     if not boxes or not isinstance(boxes, list):
         return False
