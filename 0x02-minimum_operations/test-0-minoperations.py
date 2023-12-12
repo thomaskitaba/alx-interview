50545863e6a085a20cfc7ab679b43972bf3fd068
thomaskitaba <thomaskitaba@gmail.com>
2023-12-12 11:30:31 +0300

    final_push

--- 0x02-minimum_operations/test-0-minoperations.py
+++ 0x02-minimum_operations/test-0-minoperations.py
@@ -26,3 +26,13 @@ def minOperations(n):
             op_count += 1
             total_copied += copied_all
     return op_count
+
+
+minOperations = __import__('0-minoperations').minOperations
+
+n = 4
+
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 8
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
