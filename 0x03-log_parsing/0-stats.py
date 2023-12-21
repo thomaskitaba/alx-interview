7de8f38f0700ea5b7b6f26b5c1deb4656ebbc036
thomaskitaba <thomaskitaba@gmail.com>
2023-12-21 13:42:35 +0300

    pylint-corrected

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -10,38 +10,32 @@ def print_data(statusCode, fileSize):
         if value != 0:
             print(f"{key}: {value}")
 
+count = 0
+parsed = []
+sorted_status_code = {}
+fileSize = 0
+statusCode = {"200": 0,
+              "301": 0,
+              "400": 0,
+              "401": 0,
+              "403": 0,
+              "404": 0,
+              "405": 0,
+              "500": 0}
+try:
+  for line in sys.stdin:
+      parsed = line.split()
+      if len(parsed) > 2:
+          count += 1
+          if count <= 10:
+              fileSize += int(parsed[-1])
+              code = parsed[-2]
 
-def parse_log():
-    count = 0
-    parsed = []
-    sorted_status_code = {}
-    fileSize = 0
-    statusCode = {"200": 0,
-                  "301": 0,
-                  "400": 0,
-                  "401": 0,
-                  "403": 0,
-                  "404": 0,
-                  "405": 0,
-                  "500": 0}
-    try:
-        for line in sys.stdin:
-
-            if len(parsed) > 2:
-                count += 1
-                if count < 10:
-                    fileSize += int(parsed[-1])
-                    code = parsed[-2]
-
-                    if code in statusCode.keys():
-                        statusCode[code] += 1
-                if count == 10:
-                    print_data(statusCode, fileSize)
-                    count = 0
-    finally:
-        print_data(statusCode, fileSize)
-        count = 0
-
-
-if __name__ == "__main__":
-    parse_log()
+              if code in statusCode.keys():
+                  statusCode[code] += 1
+          if count == 10:
+              print_data(statusCode, fileSize)
+              count = 0
+finally:
+  print_data(statusCode, fileSize)
+  count = 0
