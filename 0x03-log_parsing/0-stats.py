4a6d2a5093a67bfc9a80344a76c241df4bf3825c
thomaskitaba <thomaskitaba@gmail.com>
2023-12-21 13:11:04 +0300

    pylint-corrected

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,58 +1,54 @@
 #!/usr/bin/python3
-"""A short function for use with 0-generator.py """
+
 import sys
 
 
-def parse_data():
+def print_msg(dict_sc, total_file_size):
     """
-    log parse
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
     """
 
-    count = 0  # Tracks 10 iterations for printing
-    codes = []  # All valid status codes received
-    statuscodes = [200, 301, 400, 401, 403, 404, 405, 500]  # Valid codes
-    filesizes = []  # list of file sizes received
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    try:
-        for line in sys.stdin:  # Reading lines from stdin
-            if line == "":  # if empty line, skip
-                continue
-            count += 1
-            parsing = line.split()  # Break up line by spaces
-            # This is a hacky way to check format of stdin
-            if len(parsing) < 2:
-                continue
-            if not (parsing[-1].isnumeric()):
-                continue
-            if not (parsing[-2].isnumeric()):
-                continue
-            filesizes.append(int(parsing[-1]))  # last element is filesize
-            if int(parsing[-2]) in statuscodes:
-                # second to last is status code
-                codes.append(int(parsing[-2]))
-            if count % 10 == 0:  # Every ten rounds print data
-                # sumall file sizes
-                print("File size: {}".format(sum(filesizes)))
-                # dict of codes and count
-                codes_dict = {i: codes.count(i) for i in codes}
-                for k in sorted(codes_dict.keys()):
-                    print("{}: {}".format(k, codes_dict[k]))
-        # we leave the loop if we run out of lines
-        # If we didn't just print we need to
-        if count % 10 != 0:
-            print("File size: {}".format(sum(filesizes)))
-            codes_dict = {i: codes.count(i) for i in codes}
-            for k in sorted(codes_dict.keys()):
-                print("{}: {}".format(k, codes_dict[k]))
-        elif count == 0:
-            print("File size: {}".format(sum(filesizes)))
-    # If a ctrl C is sent, final print and quit
-    except KeyboardInterrupt:
-        print("File size: {}".format(sum(filesizes)))
-        codes_dict = {i: codes.count(i) for i in codes}
-        for k in sorted(codes_dict.keys()):
-            print("{}: {}".format(k, codes_dict[k]))
-        return
 
-if __name__ == '__main__':
-    parse_data()
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
