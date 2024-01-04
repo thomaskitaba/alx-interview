0a566839d7b1dcd4ea48673e6672f4f58e3d0599
thomaskitaba <thomaskitaba@gmail.com>
2024-01-04 15:32:27 +0300

    update

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -15,28 +15,29 @@
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding"""
-    dataLength = len(data)
+    """ Number of bytes in the current UTF-8 character """
     num_bytes = 0
-    for num in data:
 
+    for num in data:
+        # Check if it's the start of a new character
         if num_bytes == 0:
+            # Count the number of bytes in this character
             if (num >> 5) == 0b110:
                 num_bytes = 2
-                if (num_bytes != dataLength):
-                    return False
             elif (num >> 4) == 0b1110:
                 num_bytes = 3
-                if (num_bytes != dataLength):
-                    return False
             elif (num >> 3) == 0b11110:
                 num_bytes = 4
-                if (num_bytes != dataLength):
-                    return False
+            # If it's a single byte character, move to the next integer
             elif (num >> 7) != 0:
                 return False
         else:
+            # If the current byte doesn't start with 10, it's invalid
             if (num >> 6) != 0b10:
                 return False
             num_bytes -= 1
-    return True
+
+    if num_bytes != 0:
+        return False
+    else:
+        return True
