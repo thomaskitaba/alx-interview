78723d74c9134c105d0c824d0098ce4f1e640859
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-11 17:00:23 +0300

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,55 +1,55 @@
 #!/usr/bin/python3
+"""Solving N Queens with Backtracing"""
 import sys
 
 
-def print_solution(queens_positions):
-    """Prints the queen positions in the specified format"""
-    print([[i, j] for i, j in enumerate(queens_positions)])
+def nqueens(n, y, board):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
-def is_safe(queens_positions, row, col):
-    """Check if it's safe to place a queen in the given position"""
-    for i in range(row):
-        if queens_positions[i] == col or \
-          queens_positions[i] - i == col - row or \
-          queens_positions[i] + i == col + row:
-            return False
-    return True
-
-
-def solve_n_queens(queens_positions, row, n):
-    """Recursively solve the N queens problem"""
-    if row == n:
-        print_solution(queens_positions)
-        return
-
-    for col in range(n):
-        if is_safe(queens_positions, row, col):
-            queens_positions[row] = col
-            solve_n_queens(queens_positions, row + 1, n)
-
-
-def n_queens(n):
-    """Wrapper function to initiate the N queens solution"""
-    queens_positions = [-1] * n
-    solve_n_queens(queens_positions, 0, n)
-
-
-def handle_command_line_arguments():
-    """Handles command-line arguments and validates input"""
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: nqueens N (where N is an integer greater"
-              "than or equal to 4)")
-        exit(1)
-
-    n = int(sys.argv[1])
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
     if n < 4:
         print("N must be at least 4")
-        exit(1)
+        sys.exit(1)
 
-    return n
+    nqueens(n, 0, [])
 
 
-if __name__ == "__main__":
-    N = handle_command_line_arguments()
-    n_queens(N)
+if __name__ == '__main__':
+    main()
