ce31973a82dc6160f430835875b270623a2d4597
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-11 19:42:44 +0300

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,55 +1,57 @@
-#!/usr/bin/env python3
-"""Solving N Queens with Backtracing"""
+#!/usr/bin/python3
+""" N queens """
+
 import sys
 
 
-def nqueens(n, y, board):
+def handle_command_line_arguments():
+    """ Handles command-line arguments and validates the input """
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: nqueens N (where N is an integer greater "
+              "than or equal to 4)")
+        exit(1)
+
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    return n
+
+
+def find_queen_positions(n, i=0, a=[], b=[], c=[]):
+    """ Finds possible queen positions on the
+      chessboard using backtracking
     """
-    Method: nqueens - place n queens
-            on an n by n board so that
-            no queens are attacking any
-            others.
-    Parameters: n is an int that sets
-                board size and # of queens
-    Return: All possible solutions to
-            placement, in list of lists form
-    """
-    for x in range(n):
-        hold = 0
-        for q in board:
-            if x == q[1]:
-                hold = 1
-                break
-            if y - x == q[0] - q[1]:
-                hold = 1
-                break
-            if x - q[1] == q[0] - y:
-                hold = 1_
-                break
-        if hold == 0:
-            board.append([y, x])
-            if y != n - 1:
-                nqueens(n, y + 1, board)
-            else:
-                print(board)
-            del board[-1]
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from find_queen_positions(n, i + 1, a + [j], b + [i + j],
+                                                c + [i - j])
+    else:
+        yield a
+
+
+def solve_and_print_solutions(n):
+    """ Solves the N-queens problem and prints the solutions """
+    solutions = []
+    solution_index = 0
+
+    for queen_positions in find_queen_positions(n, 0):
+        for column in queen_positions:
+            solutions.append([solution_index, column])
+            solution_index += 1
+
+        print(solutions)
+        solutions = []
+        solution_index = 0
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    try:
-        n = int(sys.argv[1])
-    except Exception:
-        print('N must be a number')
-        sys.exit(1)
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    nqueens(n, 0, [])
+    """ Main function to execute the program """
+    n = handle_command_line_arguments()
+    solve_and_print_solutions(n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
