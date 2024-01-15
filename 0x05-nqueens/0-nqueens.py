5129405d7950305c94d41f5b31e30d98feda43c5
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-15 21:43:06 +0300

    update

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,52 +1,57 @@
 #!/usr/bin/python3
+""" N queens """
+
 import sys
 
 
-def valid_VHD(ans, row, col):
+def handle_command_line_arguments():
+    """ Handles command-line arguments and validates the input """
+    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
+        print("Usage: nqueens N (where N is an integer greater "
+              "than or equal to 4)")
+        exit(1)
 
-    for row_ans in ans:
-        # check horizontal
-        if row_ans[0] == row or row_ans[1] == col:
-            return False
-        # check if diagornal
-        if abs(row - row_ans[0]) == abs(col - row_ans[1]):
-            is_diag = True
-            return False
-    return True
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    return n
 
 
-def set_queen(N):
-    row = 0
-    col = 1
-    ans = []
+def find_queen_positions(n, i=0, a=[], b=[], c=[]):
+    """ Finds possible queen positions on the
+      chessboard using backtracking
+    """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from find_queen_positions(n, i + 1, a + [j], b + [i + j],
+                                                c + [i - j])
+    else:
+        yield a
 
-    def backtrack(row):
-        if row == N:
-            print(ans)
-            return
-        for col in range(N):
-            if valid_VHD(ans, row, col):
-                ans.append([row, col])  # decision made
-                backtrack(row + 1)  # decision point | explore the consequence
-                ans.pop()
-        if col == N:
-            return
-    backtrack(0)
+
+def solve_and_print_solutions(n):
+    """ Solves the N-queens problem and prints the solutions """
+    solutions = []
+    solution_index = 0
+
+    for queen_positions in find_queen_positions(n, 0):
+        for column in queen_positions:
+            solutions.append([solution_index, column])
+            solution_index += 1
+
+        print(solutions)
+        solutions = []
+        solution_index = 0
+
+
+def main():
+    """ Main function to execute the program """
+    n = handle_command_line_arguments()
+    solve_and_print_solutions(n)
 
 
 if __name__ == "__main__":
-    """ check number of arguments """
-    N = 1
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    """ check number of queens """
-    try:
-        N = int(sys.argv[1])
-        if int(sys.argv[1]) < 4:
-            print("N must be at least 4")
-            sys.exit(1)
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-    set_queen(N)
+    main()
