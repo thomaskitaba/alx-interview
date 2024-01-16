6c73fe251fc775d0ec60d38cbe63d3fcc6b2082f
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-16 14:02:32 +0300

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,17 @@
-#!/usr/bin/python3
+#!/usr/bin/node
+
+const request = require("request");
+const id = process.argv[2];
+const endPoint = "https://swapi-api.alx-tools.com/api/films/";
+
+request(endPoint + id, function(err, response, body) {
+  if (err) {
+    console.log(err);
+  }
+  else if(response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  }
+  else {
+    console.log("An error occured. Status code: " + response.statusCode);
+  }
+});
