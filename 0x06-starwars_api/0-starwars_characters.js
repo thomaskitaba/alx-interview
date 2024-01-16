812b47f91d239024bc2707acf27b2262fa30e8a3
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-16 15:04:46 +0300

    corrected artists varibale

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,33 +2,29 @@
 
 const request = require('request');
 const id = process.argv[2];
-const endPoint = "https://swapi-api.alx-tools.com/api/films/";
+const endPoint = 'https://swapi-api.alx-tools.com/api/films/';
 
-request(endPoint + id, function(err, response, body) {
+request(endPoint + id, function (err, response, body) {
   if (err) {
     console.log(err);
-  }
-  else if(response.statusCode === 200) {
+  } else if (response.statusCode === 200) {
     //
-    artists = JSON.parse(body).characters;
+    const artists = JSON.parse(body).characters;
     artists.map((artist) => printCharacter(artist));
-   }
-  else {
-    console.log("An error occured. Status code: " + response.statusCode);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
 
-function printCharacter(actor) {
+function printCharacter (actor) {
   // console.log(actors);
-  request (actor, function(err, response, body) {
+  request(actor, function (err, response, body) {
     if (err) {
       console.log(err);
-    }
-    else if (response.statusCode === 200) {
+    } else if (response.statusCode === 200) {
       console.log(JSON.parse(body).name);
-    }
-    else {
-      console.log("problem occured" + response.statusCode);
+    } else {
+      console.log('problem occured' + response.statusCode);
     }
   });
 }
