c07bdc8590170c40b475d8fdd2fcdf091a59220e
thomaskitaba <thomas.kitaba@gmail.com>
2024-01-29 14:31:22 +0300

    inseption_of_coin_change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+  Given a pile of coins of different values,
+  determine the fewest number of coins needed
+  to meet a given amount total.
+"""
+
+
+def makeChange(coins, total):
+    """
+      coins: list of values of the coins in our possession
+      total: the change amount in question
+    """
+    pass
+    pass
