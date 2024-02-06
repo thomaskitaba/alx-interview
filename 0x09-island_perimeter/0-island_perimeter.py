2efd6a8f51bc68f5a862b076927294651e3948d7
thomaskitaba <thomas.kitaba@gmail.com>
2024-02-06 11:52:27 +0300

    update

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,27 +14,29 @@
 
 def island_perimeter(grid):
     """ add number of land sides """
+
     height = len(grid)
     width = len(grid[0])
     perimeter = 0
-    for row in range(height):
-        for col in range(width):
-            if grid[row][col] == 1:
-                # check top of land
-                if row > 0:
-                    if grid[row - 1][col] == 0:
-                        perimeter += 1
-                #  check bottom of land
-                if row < height - 1:
-                    if grid[row + 1][col] == 0:
-                        perimeter += 1
-                # check left of land
-                if col > 0:
-                    if grid[row][col - 1] == 0:
-                        perimeter += 1
-                #  check right of land
-                if col < width - 1:
-                    if grid[row][col + 1] == 0:
-                        perimeter += 1
+    if (height <= 100 and width <= 100):
+        for row in range(height):
+            for col in range(width):
+                if grid[row][col] == 1:
+                    # check top of land
+                    if row > 0:
+                        if grid[row - 1][col] == 0:
+                            perimeter += 1
+                    #  check bottom of land
+                    if row < height - 1:
+                        if grid[row + 1][col] == 0:
+                            perimeter += 1
+                    # check left of land
+                    if col > 0:
+                        if grid[row][col - 1] == 0:
+                            perimeter += 1
+                    #  check right of land
+                    if col < width - 1:
+                        if grid[row][col + 1] == 0:
+                            perimeter += 1
 
-    return (perimeter)
+        return (perimeter)
