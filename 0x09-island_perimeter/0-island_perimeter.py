888c1a2cb84ab3214e52d93b7b6e2db33d966eb8
thomaskitaba <thomas.kitaba@gmail.com>
2024-02-06 12:03:41 +0300

    added comment \'# remove this if grid is surrounded by land"

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -27,24 +27,28 @@ def island_perimeter(grid):
                         if grid[row - 1][col] == 0:
                             perimeter += 1
                     else:
+                        # remove this if grid is surrounded by land
                         perimeter += 1
                     #  check bottom of land
                     if row < height - 1:
                         if grid[row + 1][col] == 0:
                             perimeter += 1
                     else:
+                        # remove this if grid is surrounded by land
                         perimeter += 1
                     # check left of land
                     if col > 0:
                         if grid[row][col - 1] == 0:
                             perimeter += 1
                     else:
+                        # remove this if grid is surrounded by land
                         perimeter += 1
                     #  check right of land
                     if col < width - 1:
                         if grid[row][col + 1] == 0:
                             perimeter += 1
                     else:
+                        # remove this if grid is surrounded by land
                         perimeter += 1
 
         return (perimeter)
